a006d5c76c287291f8e81264fbbfc5624d901b09
Yu Dayun <n01761548@humber.ca>
2026-09-21 14:08:23 -0400

    Remove Snippet.java and fix package issue in Student.java

--- Snippet.java
+++
@@ -1,8 +0,0 @@
-package assign1;
-
-public class Snippet {
-	public static void main(String[] args) {
-		package assign1;
-	}
-}
-
--- Student.java
+++ Student.java
@@ -1,22 +1,20 @@
-package assign1;
-//Extends all common data from Persons class into Student class
+//Extends all common data from Person class into Student class
 public class Student extends Person {
-	
-	
+
 	//firstName, lastName, courseName, courseHours is inherited from Person class
 	public Student(String firstName, String lastName, String courseName, int courseHours) {
 		super(firstName, lastName, courseName, courseHours);
-		// TODO Auto-generated constructor stub
 	}
-	
+
 	//Override method for calculating totalHours
 	@Override
 	public int getParticipatingHours(int courseHours) {
 		/*Utilizing labHours from TA to calculate total_hours*/
-		int labHours = (courseHours > 4) ? 2 :1;
+		int labHours = (courseHours > 4) ? 2 : 1;
 		int totalHours = courseHours + labHours;
 		return totalHours;
 	}
+
 	/*getTitle returns the title of Student*/
 	@Override
 	public String getTitle() {
