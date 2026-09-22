e3540b63edbef6501b1efc7fc65739964fb03617
Yu Dayun <n01761548@humber.ca>
2026-09-22 14:34:28 -0400

    Clean up comments in Professor.java

--- Professor.java
+++ Professor.java
@@ -1,20 +1,18 @@
+//Extends all common data from Person class into Student, Professor, and TA class
+public class Professor extends Person {
 
-//Extends all common data from Persons class into Student,Professor, and TA class
-public class Professor extends Person{
-	
 	//Professor constructor for Person super class variables
 	public Professor(String firstName, String lastName, String courseName, int courseHours) {
 		super(firstName, lastName, courseName, courseHours);
-		// TODO Auto-generated constructor stub
 	}
-	
+
 	/*This override function returns lecture hours*/
 	@Override
 	public int getParticipatingHours(int courseHours) {
-		/*Utilizing labHours from TA to calculate lectureHours*/
-		int labHours = (courseHours > 4) ? 2 :1;
+		//Lab hours is 2 if course is more than 4 hours, otherwise 1
+		int labHours = (courseHours > 4) ? 2 : 1;
 		int lectureHours = courseHours - labHours;
-		
+
 		return lectureHours;
 	}
 
